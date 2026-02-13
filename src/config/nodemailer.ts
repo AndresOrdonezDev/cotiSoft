75265a4dc4071d19d8129257e12532db2c8dcb6c
AndresOrdonezDev <andres93afa@hotmail.com>
2026-02-13 10:45:28 -0500

    try fix nodemailer

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,13 +1,17 @@
 import nodemailer from 'nodemailer';
 
-export function createTransport(host: string, port: number, user: string, pass: string) {
-    return nodemailer.createTransport({
-        host,
-        port,
-        secure: port === 465, // true para puerto 465, false para otros
-        auth: {
-            user,
-            pass
-        }
-    });
+export function createTransport(host:string,port:number,user:string,pass:string) {
+  return nodemailer.createTransport({
+    host,
+    port,
+    secure:true,
+    auth: {
+        user,
+        pass
+    },
+
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
 }
